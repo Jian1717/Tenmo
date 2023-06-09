1a1f3ad0f5ec23b00c327ffe08f1517c528b488d
Ezequiel <ezequiel.n85@gmail.com>
2023-06-09 04:19:01 -0400

    added the Account model on the client side because i was working with model in server side and that happen in real world

---
+++ tenmo-client/src/main/java/com/techelevator/tenmo/model/Account.java
@@ -0,0 +1,14 @@
+package com.techelevator.tenmo.model;
+
+public class Account {
+    private int account_id;
+    private double balance;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public int getAccount_id() {
+        return account_id;
+    }
+}
